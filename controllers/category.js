f771918685b7f970913947c694e6b59cb5dbd84b
Bilal Ashraf <bilalashraf6233@gmail.com>
2022-12-20 01:02:36 +0500

    server fixes

--- controllers/category.js
+++ controllers/category.js
@@ -1,38 +1,30 @@
-const Category = require('../modals/category')
-const formidable = require('formidable');
-const fs = require('fs');
-const _ = require('lodash');
-const { isError } = require('lodash');
+const Category = require("../modals/category");
+const formidable = require("formidable");
+const fs = require("fs");
+const _ = require("lodash");
+const { isError } = require("lodash");
 
-exports.catById = (req, res, next, id)=>{
-    Category.findById(id)
-    .exec((err , category)=>{
-        
-        if(err) return res.status(404).json({error: "category not found"})
-        req.catById = category
-        next()
-    })
+exports.catById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err) return res.status(404).json({ error: "category not found" });
+    req.catById = category;
+    next();
+  });
+};
 
-}
+exports.getCategoryPhoto = (req, res) => {
+  if (req.catById.img.data) {
+    res.set("Content-Type", req.catById.img.contentType);
+    return res.send(req.catById.img.data);
+  }
+};
 
-exports.getCategoryPhoto = (req, res)=>{
- if(req.catById.img.data){
-     res.set("Content-Type", req.catById.img.contentType)
-     return res.send(req.catById.img.data)
- }
-
-
-
-}
-
-
-exports.getCategories =  (req, res)=>{
- 
-    Category.find({}, (err,categories)=>{
-        if(err) return res.status(404).json({error: 'No Record Found...!'})
-        return res.status(200).json({categories: categories})
-    }).select('-img')
-}
+exports.getCategories = (req, res) => {
+  Category.find({}, (err, categories) => {
+    if (err) return res.status(404).json({ error: "No Record Found...!" });
+    return res.status(200).json({ categories: categories });
+  }).select("-img");
+};
 
 // exports.createcategory = (req, res)=>{
 //     const category = new Category(req.body)
@@ -46,74 +38,69 @@ exports.getCategories =  (req, res)=>{
 //     })
 // }
 
-exports.createcategory = (req, res)=>{
-    let form = new formidable.IncomingForm()
-    
-    form.keepExtensions = true; 
+exports.createcategory = (req, res) => {
+  let form = new formidable.IncomingForm();
 
-    form.parse(req, (err, fields, files)=>{
-        console.log('error', err, fields, files)
-        if(err){
-            return res.status(405).json({error: 'Picture does not Uploded'})
-        }
-        let category = new Category(fields)
-        if(files.img){
-            category.img.data = fs.readFileSync(files.img.path)
-            category.contentType = files.img.type
+  form.keepExtensions = true;
 
-            console.log('category is here', category)
-        }
+  form.parse(req, (err, fields, files) => {
+    console.log("error", err, fields, files);
+    if (err) {
+      return res.status(405).json({ error: "Picture does not Uploded" });
+    }
+    let category = new Category(fields);
+    if (files.img) {
+      category.img.data = fs.readFileSync(files.img.path);
+      category.img.contentType = files.img.type;
+    }
 
-        category.save((err, results)=>{ 
-            
-            if(err){
-                return res.status(403).json({error: 'category not saved'})
-            }
-             return res.status(200).json({results})
-        })
-    })
-
-}
+    category.save((err, results) => {
+      if (err) {
+        return res.status(403).json({ error: "category not saved" });
+      }
+      return res.status(200).json({ results });
+    });
+  });
+};
 
 // UPDATE CATEGORY
 
-exports.updateCategory = (req, res)=>{
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
-    
-    form.parse(req, (err, fields, files)=>{
-        if(err) return res.status(400).json({error: 'failed to parse Request'})
-        let category = req.catById
-        category = _.extend(category, fields)
-        category.updated =  Date()
+exports.updateCategory = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
 
-        if(files.img){
-            category.img.data = fs.readFileSync(files.img.path)
-            category.img.contentType = files.img.type
-        }
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(400).json({ error: "failed to parse Request" });
+    let category = req.catById;
+    category = _.extend(category, fields);
+    category.updated = Date();
 
-        category.save((err, result)=>{
-            if(err) return res.status(400).json({error: 'failed to Update Category'})
-
-            
-                return res.status(200).json({category})
-            
-        })
-    })
-}
-
-exports.getCategory = (req, res)=>{
-    if(req.catById){
-        return res.status(200).json({category: req.catById})
+    if (files.img) {
+      category.img.data = fs.readFileSync(files.img.path);
+      category.img.contentType = files.img.type;
     }
-    return res.status(400).json({error: 'No Record Found...!'})
-}
 
-exports.deletecategory = (req, res)=>{
-    let catById = req.catById
-    catById.remove((err, category)=>{
-        if(err) return res.status(400).json({error: 'Category Not Deleted'})
+    category.save((err, result) => {
+      if (err)
+        return res.status(400).json({ error: "failed to Update Category" });
 
-        return res.status(200).json({category: category})
-    })
-}
+      return res.status(200).json({ category });
+    });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  if (req.catById) {
+    return res.status(200).json({ category: req.catById });
+  }
+  return res.status(400).json({ error: "No Record Found...!" });
+};
+
+exports.deletecategory = (req, res) => {
+  let catById = req.catById;
+  catById.remove((err, category) => {
+    if (err) return res.status(400).json({ error: "Category Not Deleted" });
+
+    return res.status(200).json({ category: category });
+  });
+};
--- controllers/product.js
+++ controllers/product.js
@@ -1,139 +1,128 @@
-const Product = require('../modals/product')
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Types.ObjectId
-const formidable = require('formidable');
-const fs = require('fs');
-const _ = require('lodash');
+const Product = require("../modals/product");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
+const formidable = require("formidable");
+const fs = require("fs");
+const _ = require("lodash");
 
+exports.getProducts = (req, res, next) => {
+  Product.find({}, (err, products) =>
+    res.status(200).json({ products })
+  ).select("-img");
+};
 
-exports.getProducts = (req, res, next)=>{
-    
-    Product.find({}, (err, products) => res.status(200).json({products}))
-    .select('-img')
-    
-}
+exports.deleteProduct = (req, res) => {
+  if (req.productById) {
+    let product = req.productById;
+    product.remove((err, product) => {
+      if (err)
+        return res.status(400).json({ error: "Product Not Deleted...!" });
+      return res
+        .status(200)
+        .json({ message: "Product Deleted Successfully...!" });
+    });
+  }
+};
 
-exports.deleteProduct = (req, res)=>{
-    if(req.productById){
-        let product = req.productById
-        product.remove((err, product)=>{
-            if(err) return res.status(400).json({error: 'Product Not Deleted...!'})
-            return res.status(200).json({message: 'Product Deleted Successfully...!'}) 
-        })
-    }
-}
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err) return res.status(404).json({ error: "Product Not Found" });
 
-exports.productById = (req, res, next, id)=>{
-    Product.findById(id)
-    .exec(( err, product)=>{
-        if(err) return res.status(404).json({error: 'Product Not Found'})
+    req.productById = product;
+    next();
+  });
+};
 
-        req.productById = product
-        next()
-    })
-} 
+exports.getProductById = (req, res) => {
+  if (req.productById) {
+    return res.status(200).json({ product: req.productById });
+  }
 
-exports.getProductById = (req, res)=>{
-    if(req.productById){
-        return res.status(200).json({product: req.productById})
-    }
+  return res.status(404).json({ error: "Product not found" });
+};
 
-    return res.status(404).json({error: 'Product not found'})
-}
+exports.findWithCategoryId = (req, res) => {
+  if (req.productByCatId) {
+    return res.status(200).json({ products: req.productByCatId });
+  }
+};
 
-exports.findWithCategoryId = (req, res)=>{
-    if(req.productByCatId){
-        return res.status(200).json({products: req.productByCatId})
-    }
-}
+exports.productByCatId = (req, res, next, id) => {
+  Product.find({ categoryId: ObjectId(id) }, (err, products) => {
+    if (err)
+      return res.status(404).json({ error: "Category Not Exist with this Id" });
 
-exports.productByCatId = (req, res, next, id)=>{
-    Product.find({categoryId: ObjectId(id)}, (err, products)=>{
-
-        if(err) return res.status(404).json({error: 'Category Not Exist with this Id'})
-
-       req.productByCatId = products
-       next()
-    }).select('-__v -img')
-    
-    
-}
-
+    req.productByCatId = products;
+    next();
+  }).select("-__v -img");
+};
 
 //  PRODUCT PHOTO
 
-exports.getProductPhoto = (req, res)=>{
-    console.log('request come to get product photo')
-    if(req.productById.img.data){
-         res.set("Content-Type", req.productById.img.contentType)
-         return res.send(req.productById.img.data)
-     }
-}
+exports.getProductPhoto = (req, res) => {
+  console.log("request come to get product photo");
+  if (req.productById.img.data) {
+    res.set("Content-Type", req.productById.img.contentType);
+    return res.send(req.productById.img.data);
+  }
+};
 
 // UPDATE PRODUCT
 
-exports.updateProduct = (req, res) =>{
+exports.updateProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
 
-    let form = new formidable.IncomingForm();
-    form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return res.status(400).json({ error: "failed to parse update request" });
 
-    form.parse(req, (err, fields, files)=>{
-        if(err) return res.status(400).json({error: 'failed to parse update request'})
-        
-        let product = req.productById
-        product = _.extend(product, fields)
-        product.updated = Date.now()
+    let product = req.productById;
+    product = _.extend(product, fields);
+    product.updated = Date.now();
 
-        if(files.img){
-            category.img.data = fs.readFileSync(files.img.path)
-            category.img.contentType = files.img.type
-        }
+    if (files.img) {
+      category.img.data = fs.readFileSync(files.img.path);
+      category.img.contentType = files.img.type;
+    }
 
-        product.save((err, results)=>{
-            if(err) return res.status(400).json({error: 'failed to update'})
+    product.save((err, results) => {
+      if (err) return res.status(400).json({ error: "failed to update" });
 
-            return res.status(200).json({product})
-        })
-    })
-}
+      return res.status(200).json({ product });
+    });
+  });
+};
 
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
 
-exports.createProduct = (req, res)=>{
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(405).json({ error: "Picture does not Uploded" });
+    let product = new Product(fields);
 
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true;
-    
-    form.parse(req, (err, fields, files)=>{
-        if(err) return res.status(405).json({error: 'Picture does not Uploded'})
-        let product = new Product(fields)
+    if (files.img) {
+      product.img.data = fs.readFileSync(files.img.path);
+      product.img.contentType = files.img.type;
+    }
 
-        if(files.img){
-            product.img.data = fs.readFileSync(files.img.path)
-            product.contentType = files.img.type
-        }
+    console.log("product", product);
+    product.save((err, results) => {
+      if (err) {
+        return res.status(405).json({ error: err });
+      }
+      return res.status(200).json({ message: "Product Saved" });
+    });
+  });
 
-        
+  // const product = new Product(req.body)
 
-        console.log('product', product)
-        product.save((err, results)=>{
-            if(err){
-                return res.status(405).json({error:  err})
-            }
-             return res.status(200).json({message: 'Product Saved'})
-        })
-    })
+  // product.save((err, product)=>{
+  //     if(err || !product){
+  //         return res.status(405).json({error: err})
+  //     }
 
-
-
-    
-
-    // const product = new Product(req.body)
-
-    // product.save((err, product)=>{
-    //     if(err || !product){
-    //         return res.status(405).json({error: err})
-    //     }
-
-    //     return res.status(200).json({product: product})
-    // })
-}
+  //     return res.status(200).json({product: product})
+  // })
+};
